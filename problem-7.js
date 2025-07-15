0496e6ad927d8fdba81d02f2f9fd63dce50135be
prantonub <prantonub.cse23@gmail.com>
2025-07-15 23:55:18 +0600

    task added

---
+++ problem-7.js
@@ -0,0 +1,10 @@
+// // Free Movie Ticket Offer
+// If someone buys tickets worth more than 1000 taka, they get a free popcorn.
+// Otherwise, popcorn costs 50 taka.
+const ticketPrice = 1200;
+
+if (ticketPrice > 1000) {
+    console.log("Free popcorn");
+} else {
+    console.log("Buy popcorn for 50tk");
+}
